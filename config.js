b34413cea8ada0b457f28ebe96c7e9cf10231218
Anders Fisher <anders@devmountain.co.uk>
2015-12-20 18:00:39 +0000

    Replace conifg.js with a sample config file

--- config.js
+++
@@ -1,4 +0,0 @@
-// the uuid below is for @atleastimtrying's sphero bb8, you'll need to replace this with your own.
-module.exports = {
-  uuid: "cb157b931d5a4066bdc73de6080b794b"
-};
---
+++ sample-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  uuid: "insert uuid here",
+  debugger: true
+};
+
